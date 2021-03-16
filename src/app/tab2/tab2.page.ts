fd6597ad12161bec9bd07e47643f1aa6c2c57231
Jason <c14170129@john.petra.ac.id>
2021-03-17 02:59:35 +0700

    added profile in tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  loser: boolean = true;
+  winner: boolean = false;
+  luckyNumber: number;
+  inputNumber: number;
+
   constructor() {}
 
+  ngOnInit() {
+    this.luckyNumber = Math.floor(Math.random() * 4 + 1)
+  }
+
+  checkWin(event: any) {
+    this.inputNumber = Number(event.target.value)
+    if (this.inputNumber == this.luckyNumber) {
+      this.loser = false
+      this.winner = true
+    } else {
+      this.loser = true
+      this.winner = false
+    }
+  }
 }
